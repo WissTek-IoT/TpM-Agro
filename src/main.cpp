95be9ba973b19f6ea99da16edc6df43e73b0a53d
vdrad <victor.drad.g@gmail.com>
2024-08-18 11:22:43 -0300

    fix: removed unused variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include "mot.h"
 
-unsigned long last_time = 0;
-unsigned long current_time = 0;
-
-unsigned long long begin = 0;
-unsigned long long end = 0;
-
 void setup() {
   init_mot_protocol();
 }
 
 void loop() {
-  current_time = millis();
-
   run_application();
   receive_mot_packet();
 }
